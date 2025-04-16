1db2c4d756323e8630460b80258b3d9bd33c9aff
coryfcowden <cfcowden@calpoly.edu>
2025-04-15 19:44:17 -0700

    user services mock tests

--- packages/express-backend/backend_unit_test/user-services-mock.test.js
+++ packages/express-backend/backend_unit_test/user-services-mock.test.js
@@ -7,6 +7,7 @@ let mockFind;
 let mockSave;
 let mockModel;
 let mockConnection;
+let mockFindById;
 
 beforeEach(() => {
   jest.clearAllMocks();
@@ -14,12 +15,14 @@ beforeEach(() => {
   // Set up mock functions
   mockFind = jest.fn();
   mockSave = jest.fn();
+  mockFindById = jest.fn();
 
   // Create a mock model object for static methods like .find
   mockModel = function () {
     return { save: mockSave };
   };
   mockModel.find = mockFind;
+  mockModel.findById = mockFindById;
 
   // Create a mock connection that returns the mock model
   mockConnection = {
@@ -54,6 +57,23 @@ test("adding user", async () => {
 
   expect(result).toEqual(newUser);
   expect(mockSave).toHaveBeenCalledTimes(1);
+});
+
+test("finding user by id", async () => {
+  const user = {
+    name: "John Doe",
+    job: "Unemployed",
+    email: "desperatelyneedjob@example.com"
+  };
+
+  const mockId = "1234abcd";
+
+  mockFindById.mockResolvedValue(user);
+
+  const result = await userServices.findUserById(mockId);
+
+  expect(result).toEqual(user);
+  expect(mockFindById).toHaveBeenCalledTimes(1);
 });
 
 test("finding user by name", async () => {
@@ -108,10 +128,32 @@ test("finding user by job", async () => {
   expect(mockFind).toHaveBeenCalledTimes(1);
 });
 
+test("finding user by invalid id --> null", async () => {
+  mockFindById.mockResolvedValue(null);
+
+  const result = await userServices.findUserById("invalidID");
+
+  expect(result).toBeNull();
+});
+
 test("invalid name", async () => {
   const newUser = {
     job: "Software Engineer",
     email: "jdoe@example.com"
+  };
+
+  mockSave.mockResolvedValue(newUser);
+
+  const result = await userServices.addUser(newUser);
+
+  expect(result).toBe(false);
+  expect(mockSave).toHaveBeenCalledTimes(0);
+});
+
+test("invalid email", async () => {
+  const newUser = {
+    name: "John Doe",
+    job: "Software Engineer"
   };
 
   mockSave.mockResolvedValue(newUser);
